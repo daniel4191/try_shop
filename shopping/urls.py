5a506413c178c91ab911fe324c68e6ba812338de
daniel4191 <daniell8282@naver.com>
2023-01-16 21:34:49 +0900

    stopped by many errors in reference

--- shopping/urls.py
+++ shopping/urls.py
@@ -24,10 +24,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('shop/', include('shop.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
